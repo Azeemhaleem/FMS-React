57a604dd81d4f06019a0f9d7d60518dc36604266
Sandali Nanayakkara <shelasandali@gmail.com>
2025-09-05 11:49:44 +0530

    update import in backend to solve appeal issue

--- FMS-backend/app/Http/Controllers/driver/FineAppealController.php
+++ FMS-backend/app/Http/Controllers/driver/FineAppealController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\driver;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\ChargedFine;
 use App\Models\FineAppealRequest;
